cb6552c8402a620afd7b8a6311020304c0929fbd
gaozhengyang <gaozhengyang@gaozhengyang>
2016-09-01 10:59:34 +0800

    learn java something

---
+++ src/test/java/test/HelloWorld.java
@@ -0,0 +1,32 @@
+/**
+* Copyright (C) 2016, wangao110@hotmail.com All Rights Reserved.
+*/
+package test;
+		
+/**
+* ClassName: HelloWorld <br/>
+*
+* @author wangao110@hotmail.com
+* @version 1.0
+* @date 2016年9月1日
+* @since JDK 1.7
+*/
+public class HelloWorld {
+
+    /**
+    * main:(这里用一句话描述这个方法的作用). <br/>
+    * TODO(这里描述这个方法适用条件 – 可选).<br/>
+    * TODO(这里描述这个方法的执行流程 – 可选).<br/>
+    * TODO(这里描述这个方法的使用方法 – 可选).<br/>
+    * TODO(这里描述这个方法的注意事项 – 可选).<br/>
+    *
+    * @author wangao110@hotmail.com
+    * @param args
+    * @since JDK 1.7
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
