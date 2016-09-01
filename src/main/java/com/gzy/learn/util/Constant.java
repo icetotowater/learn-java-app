4ff5194faa7eb07d823b3724f70d86b1d30537a5
gaozhengyang <gaozhengyang@gaozhengyang>
2016-09-01 11:15:24 +0800

    完善项目目录

---
+++ src/main/java/com/gzy/learn/util/Constant.java
@@ -0,0 +1,16 @@
+/**
+* Copyright (C) 2016, wangao110@hotmail.com All Rights Reserved.
+*/
+package com.gzy.learn.util;
+		
+/**
+* ClassName: Constant 全局常量工具类<br/>
+*
+* @author wangao110@hotmail.com
+* @version 1.0
+* @date 2016年9月1日
+* @since JDK 1.7
+*/
+public final class Constant {
+
+}
