e0424484e0308e5392e230d83474dcf1719dc8b0
gaozhengyang <wangao110@hotmail.com>
2016-09-02 09:48:54 +0800

    包框架

---
+++ src/main/java/com/gzy/learn/jdkapi/random/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+* Copyright (C) 2016, wangao110@hotmail.com All Rights Reserved.
+*/
+package com.gzy.learn.jdkapi.random;
+		
+/**
+* ClassName: HelloWorld <br/>
+*
+* @author wangao110@hotmail.com
+* @version 1.0
+* @date 2016年9月2日
+* @since JDK 1.7
+*/
+public class HelloWorld {
+
+    /**
+    *
+    * @author wangao110@hotmail.com
+    * @param args
+    * @since JDK 1.7
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
